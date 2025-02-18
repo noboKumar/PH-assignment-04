92e1de285a1eb32e9e4f4da411731b5d372766b4
noboKumar <nobokumar227@gmail.com>
2025-02-18 19:44:44 +0600

    solved problem3

--- assignment.js
+++ assignment.js
@@ -47,3 +47,20 @@ function electionResult(votes) {
     return "Draw";
   }
 }
+
+function isBestFriend(f1, f2) {
+  if (
+    typeof f1 !== "object" ||
+    f1 === null ||
+    Array.isArray(f1) ||
+    typeof f2 !== "object" ||
+    f2 === null ||
+    Array.isArray(f2)
+  ) {
+    return "Invalid";
+  } else if (f1.bestFriend === f2.roll && f2.bestFriend === f1.roll) {
+    return true;
+  } else {
+    return false;
+  }
+}
